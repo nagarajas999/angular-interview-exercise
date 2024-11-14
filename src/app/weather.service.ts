0ad8abea2de65f7ef74b3adc5a8424a4cfde4f29
Nagaraja Singamchetty <nagarajas999@gmail.com>
2024-11-14 19:45:41 +0530

    weather service code check in

---
+++ src/app/weather.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private stationsUrl = `${environment.apiUrl}?limit=100`;
+  private observationsUrl = `${environment.apiUrl}/`;
+
+  constructor(private http: HttpClient) { }
+
+  getStations(): Observable<any> {
+    return this.http.get<any>(this.stationsUrl);
+  }
+
+  getTemperature(stationId: string): Observable<any> {
+    return this.http.get<any>(`${this.observationsUrl}${stationId}/observations?limit=1`);
+  }
+}
