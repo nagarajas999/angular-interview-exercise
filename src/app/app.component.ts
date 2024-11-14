c9f3f6be66860faba6d68fa5c4edcd006a8f5fe0
Nagaraja Singamchetty <nagarajas999@gmail.com>
2024-11-14 20:27:48 +0530

    component file checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { WeatherService } from './weather.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,47 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor() {
+  stations: any[] = [];
+  selectedStation: any = null;
+  temperature: string | null = null;
+  paginatedStations: any[] = [];
+  pageSize = 10;
+  currentPage = 0;
+  title = 'ALASKA-interview-exercise';
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private weatherService: WeatherService) {
 
   }
 
   ngOnInit(): void {
-    
+    this.loadStations();
   }
 
+  loadStations(): void {
+    this.weatherService.getStations().subscribe((data) => {
+      this.stations = data.features;
+      this.updatePaginatedStations();
+    });
+  }
+
+  onSelectStation(station: any): void {
+    this.selectedStation = station;
+    this.weatherService.getTemperature(station.properties.stationIdentifier).subscribe((data) => {
+      const temperature = data.features[0].properties?.temperature?.value;
+      this.temperature = temperature ? `${temperature} °F` : 'Temperature not available';
+    });
+  }
+
+  updatePaginatedStations(): void {
+    const startIndex = this.currentPage * this.pageSize;
+    this.paginatedStations = this.stations.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  handlePageEvent(event: PageEvent): void {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.updatePaginatedStations();
+  }
 }
