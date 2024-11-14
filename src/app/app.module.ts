ac1ca9f38835bdbbad145e7e5feb0363ef2c66c0
Nagaraja Singamchetty <nagarajas999@gmail.com>
2024-11-14 20:32:46 +0530

    module file checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherService } from './weather.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({ declarations: [
         AppComponent
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+        AppRoutingModule,MatPaginatorModule], providers: [provideHttpClient(withInterceptorsFromDi()),WeatherService, provideAnimationsAsync()] })
 export class AppModule { }
